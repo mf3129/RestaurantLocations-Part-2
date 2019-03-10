c94bdcf5b9daac3887636c80f3246b56b8f8c167
Makan Fofana <mfofanaT99@gmail.com>
2019-03-09 23:50:33 -0500

    Fetching data from cloud database & placing in tableView cell using synchronous loading.

--- FoodPin/Controller/DiscoverTableViewController.swift
+++ FoodPin/Controller/DiscoverTableViewController.swift
@@ -11,6 +11,8 @@ import CloudKit
 
 class DiscoverTableViewController: UITableViewController {
 
+    var restaurants: [CKRecord] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,16 +21,17 @@ class DiscoverTableViewController: UITableViewController {
         
         //Formatting the navigation view controller
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        
         navigationController?.navigationBar.shadowImage = UIImage()
         if let customFont = UIFont(name: "Rubik-Medium", size: 40.0) {
-            navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(red: 233, green: 76, blue: 60), NSAttributedString.Key.font: customFont]
+            navigationController?.navigationBar.largeTitleTextAttributes = [ NSAttributedString.Key.foregroundColor: UIColor(red: 233, green: 76, blue: 60), NSAttributedString.Key.font: customFont ]
         }
         
         fetchRecordsFromCloud()
     }
     
     
-    //Fetching Records From ICloud Database
+    //Fetching Data From ICloud Database using convenience API
     func fetchRecordsFromCloud(){
         
         let cloudContainer = CKContainer.default()
@@ -44,78 +47,45 @@ class DiscoverTableViewController: UITableViewController {
             if let results = results {
                 print("We have completed the downloading of the Restaurant Data")
                 self.restaurants = results
-                self.tableView.reloadData()
+                
+                //Will reload the icloud Table Data on main thread
+                DispatchQueue.main.sync {
+                    self.tableView.reloadData()
+                }
             }
         }
     }
     
-    
-    
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
+    //Assigning Fetched Data to Discover Table Cells
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DiscoverCell", for: indexPath)
+        
+        //Configure the cell
+        let restaurant = restaurants[indexPath.row]
+        cell.textLabel?.text = restaurant.object(forKey: "name") as? String
+        
+        if let image = restaurant.object(forKey: "image"), let imageAsset = image as? CKAsset {
+            
+            if let imageData = try? Data.init(contentsOf: imageAsset.fileURL){
+                cell.imageView?.image = UIImage(data: imageData)
+            }
+        }
+        
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return restaurants.count
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
